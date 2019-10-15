9b3d4fbb60b2207302477838cebfb36df6cda150
Keshab <keshab.bista.2020@student.lhschools.org>
2019-10-15 09:59:45 -0400

    Started second method

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -5,11 +5,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         printGreeting();
+        printName(String);
     }
 
     public static void printGreeting(){
         System.out.println("Hello World");
     }
 
+    public static String printName(String userName){
+        Scanner keyboard;
+        keyboard = new Scanner (System.in);
+        System.out.println("What is your name?");
+        userName = keyboard.nextLine();
+        return "Hello" + userName;
+    }
 }
